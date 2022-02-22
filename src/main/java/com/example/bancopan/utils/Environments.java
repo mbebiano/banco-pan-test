c7adb480eb296c4bdb391643331f8d85983ba778
Mattheus Bebiano <mattheus.bebiano.ext@e-ai.com.br>
2022-02-22 16:19:51 -0300

    feat: Added Environments

---
+++ src/main/java/com/example/bancopan/utils/Environments.java
@@ -0,0 +1,12 @@
+package com.example.bancopan.utils;
+
+public class Environments {
+
+    public static final String VIA_CEP_API_HOST = "via.cep.api.host";
+    public static final String VIA_CEP_API_CONNECTION_TIMEOUT = "via.cep.api.connection.timeout";
+    public static final String VIA_CEP_API_READ_TIMEOUT = "via.cep.api.read.timeout";
+
+    public static final String SEVICO_DADOS_IBGE_API_HOST = "servicos.dados.ibge.api.host";
+    public static final String SEVICO_DADOS_IBGE_CONNECTION_TIMEOUT = "servico.dados.ibge.connection.timeout";
+    public static final String SEVICO_DADOS_IBGE_API_TIMEOUT = "servico.dados.ibge.api.read.timeout";
+}
