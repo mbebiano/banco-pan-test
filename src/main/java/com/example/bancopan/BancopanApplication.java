74d65181323866dbabb163737062a44858b07d21
Mattheus Bebiano <mattheus.bebiano.ext@e-ai.com.br>
2022-02-22 16:14:57 -0300

    feat: Initializing project structure

---
+++ src/main/java/com/example/bancopan/BancopanApplication.java
@@ -0,0 +1,13 @@
+package com.example.bancopan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancopanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancopanApplication.class, args);
+	}
+
+}
